424261ccbd7bde94f4b8fbf7f12a6e4b14d373f6
Pawel Galazka <p.galazka@clearcode.cc>
2013-12-06 10:56:37 +0100

    First commit, license, readme, setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+#Copyright (C) 2013-2014 by Clearcode <http://clearcode.cc>
+#and associates (see AUTHORS).
+#
+#This file is part of migopy.
+#
+#Migopy is free software: you can redistribute it and/or modify
+#it under the terms of the GNU Lesser General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#Migopy is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU Lesser General Public License for more details.
+#
+#You should have received a copy of the GNU Lesser General Public License
+#along with migopy.  If not, see <http://www.gnu.org/licenses/>.
+
+from distutils.core import setup
+
+setup(
+    name='migopy',
+    version='1.0 beta',
+    author='Pawel Galazka',
+    author_email='p.galazka@clearcode.cc',
+    packages=['migopy']
+)
+
