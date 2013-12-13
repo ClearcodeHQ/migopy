49eaac14d2957d58c42ca5e50225f6bc6974cc98
Pawel Galazka <p.galazka@clearcode.cc>
2013-12-13 09:21:30 +0100

    Additional integration test for dbdump

--- tests/test_integrations.py
+++ tests/test_integrations.py
@@ -46,6 +46,18 @@ migrations = task(Migrations.create_task())
 
 """
 
+fabfile_with_mongodump = """
+import migopy
+
+
+class Migrations(migopy.MigrationsManager):
+    MONGO_DATABASE = 'migopy_db_test'
+    DO_MONGO_DUMP = True
+
+
+migrations = Migrations.create_task()
+"""
+
 files['mongomigrations/001_test.py'] = """
 def up(db):
     pass
@@ -133,6 +145,12 @@ class MongoMigrationsIntegratedBehavior(unittest.TestCase):
     def test_it_do_fab_migrations_dbdump(self):
         self.assertEqual(len(glob('mongodumps/*')), 0)
         call('fab migrations:dbdump')
+        self.assertEqual(len(glob('mongodumps/*')), 1)
+
+    def test_it_do_dbdump_during_fab_migrations_execute(self):
+        self.assertEqual(len(glob('mongodumps/*')), 0)
+        self.tmp_dir.create_file('fabfile.py', fabfile_with_mongodump)
+        call('fab migrations:execute')
         self.assertEqual(len(glob('mongodumps/*')), 1)
 
     def test_it_works_with_fabric_task_decorator(self):
